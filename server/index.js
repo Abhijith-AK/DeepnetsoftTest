af25499c59b15008d75668a1dcc039d07815957c
Abhijith Krishna A K <138287385+Abhijith-AK@users.noreply.github.com>
2025-03-28 01:22:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,8 +10,7 @@ const app = express()
 
 // enabling cors
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+    origin: "*",
 }))
 // initializing json middleware
 app.use(express.json())
